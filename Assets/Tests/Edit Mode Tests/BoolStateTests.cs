20e32d740e38050a8ee29cb9bbbfadda7e33e1cf
Xavier Banham <20094400@tafe.wa.edu.au>
2026-02-19 12:13:00 +0800

    Worked on unit tests, and created a testing script for the BoolState class

---
+++ Assets/Tests/Edit Mode Tests/BoolStateTests.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using GOAP;
+
+public class BoolStateTests {
+    // A Test behaves as an ordinary method
+    [Test]
+    public void Construct() {
+        G_BoolState testState = ScriptableObject.CreateInstance<G_BoolState>();
+
+        testState.Construct("test", true);
+
+        Assert.AreEqual("test", testState.name);
+        Assert.AreEqual(true, (bool)testState.GetValue());
+    }
+
+    [Test]
+    public void Clone() {
+        G_BoolState testState = ScriptableObject.CreateInstance<G_BoolState>();
+        testState.Construct("test", true);
+
+        G_State cloneState = testState.Clone();
+
+        Assert.AreEqual(testState.name, cloneState.name);
+        Assert.AreEqual((bool)testState.GetValue(), (bool)cloneState.GetValue());
+        Assert.IsTrue(cloneState is G_BoolState);
+    }
+
+    [TestCase(true, G_StateComparison.equal, true, true)]
+    [TestCase(false, G_StateComparison.equal, true, false)]
+    [TestCase(true, G_StateComparison.not_equal, false, true)]
+    [TestCase(false, G_StateComparison.not_equal, false, false)]
+    public void TestState(bool actualValue, G_StateComparison comparison, bool expectedValue, bool expectedResult) {
+        G_BoolState testState = ScriptableObject.CreateInstance<G_BoolState>();
+        testState.Construct("test", actualValue);
+
+        bool result = testState.TestState(testState, comparison, expectedValue);
+
+        Assert.AreEqual(expectedResult, result);
+    }
+}
