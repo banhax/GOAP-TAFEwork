02f1f2aa0f222ce842be2cd1520aa9d20616c407
Xavier Banham <20094400@tafe.wa.edu.au>
2026-03-05 12:20:26 +0800

    made the node and node state classes, and made the goal and action constructors for G_Node

---
+++ Assets/Scripts/GOAP/Planner/G_Node.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GOAP {
+    public class G_Node {
+        #region Data
+
+        G_NodeState nodeState = G_NodeState.open;
+        public G_NodeState NodeState { get { return nodeState; } }
+
+        G_Action nodeAction;
+        public G_Action NodeAction { get { return nodeAction; } }
+
+        G_Node parentNode;
+        public G_Node ParentNode { get { return parentNode; } }
+
+        int hCost = 0; 
+        public int HCost { get { return hCost; } }
+
+        int unmetPreconditions = 0;
+        public int UnmetPreconditions { get { return unmetPreconditions; } }
+
+        List<G_Action> nodeActionPool = new List<G_Action>();
+        public List<G_Condition> preconditions = new List<G_Condition>();
+
+        G_WorldState worldStateRef;
+
+        bool isGoalNode = false;
+        public bool IsGoalNode { get { return isGoalNode; } }
+        #endregion
+
+        #region Constructors
+
+        // constructor for actions
+        public G_Node(G_Node parentNode,
+            G_Action nodeAction,
+            int hCost,
+            List<G_Action> actionPool,
+            List<G_Condition> preconditions,
+            G_WorldState worldStateRef) {
+
+            // parent node
+            this.parentNode = parentNode;
+
+            // action
+            this.nodeAction = nodeAction;
+
+            // hCost
+            this.hCost = hCost + nodeAction.GetCost();
+
+            // action pool
+            this.nodeActionPool = new List<G_Action>(nodeActionPool);
+            this.nodeActionPool.Remove(this.nodeAction);
+
+            // preconditions
+            this.preconditions = new List<G_Condition>(preconditions);
+
+            // world state reference
+            this.worldStateRef = worldStateRef;
+
+            nodeState = G_NodeState.open;
+
+            // determine unmet preconditions
+            this.unmetPreconditions = ProcessPreconditions(this.preconditions, this.worldStateRef);
+        }
+
+        public G_Node(List<G_Action> nodeActionPool,
+            List<G_Condition> preconditions,
+            G_WorldState worldStateRef) {
+
+                // actionPool
+                this.nodeActionPool = new List<G_Action>(nodeActionPool);
+
+                // preconditions
+                this.preconditions = new List<G_Condition>(preconditions);
+
+                // world state reference
+                this.worldStateRef = worldStateRef;
+
+                nodeState = G_NodeState.open;
+
+                // determine unmet preconditions
+                this.unmetPreconditions = ProcessPreconditions(this.preconditions, this.worldStateRef);
+
+                isGoalNode = true;
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Take the list of preconditions and test them against the current world state to see if they are met or not.
+        /// If a precondition is met by the world state, we will set it to Met and add 1 to the unmetCount
+        /// </summary>
+        /// <param name="preconditions"></param>
+        /// <param name="worldStateRef"></param>
+        /// <returns></returns>
+        public int ProcessPreconditions(List<G_Condition> preconditions, G_WorldState worldStateRef) {
+            int unmetCount = 0;
+
+            for (int i = 0; i < preconditions.Count; i++) {
+                if (!preconditions[i].Met) {
+                    ProcessPrecondition(preconditions[i], ref unmetCount);
+                }
+            }
+
+            return unmetCount;
+        }
+
+        void ProcessPrecondition(G_Condition precondition, ref int unmetCount) {
+            G_State stateRef = worldStateRef.states.Find((state) => precondition.IsStateTheConditionState(state));
+            if(stateRef != null && precondition.DoesStateMeetCondition(stateRef)) {
+                precondition.Meet();
+            }
+            if (!precondition.Met) {
+                unmetCount += 1;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/GOAP/Planner/G_NodeState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace GOAP {
+    public enum G_NodeState {
+        open,
+        closed,
+        fail,
+        success
+    }
+}
