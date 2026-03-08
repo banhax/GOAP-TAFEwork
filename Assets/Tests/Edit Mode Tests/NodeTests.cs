833ed9117625a30eeed3242129b075d4ccbe1594
Xavier Banham <20094400@tafe.wa.edu.au>
2026-03-08 17:34:57 +0800

    wrote some of the node tests

--- Assets/Tests/Edit Mode Tests/NodeTests.cs
+++ Assets/Tests/Edit Mode Tests/NodeTests.cs
@@ -2,10 +2,76 @@ using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using GOAP;
 
 public class NodeTests {
-    [Test]
-    public void NodeTestsSimplePasses() {
+    // constructors for normal nodes and for goal nodes
+
+    // process preconditions test - checking for fulfilled preconditions from the world state
+
+    // process node - get the node's planning result
+
+    // generate child nodes
+
+    // return plan - return the whole plan as a list
+
+    [TestCase(true, 0, 1, 1, TestName = "Goal  Node")]
+    [TestCase(TestName = "Normal Node")]
+    public void Constructor(bool testGoalNode,
+            int hCost,
+            int unmetCount,
+            int preconCount) {
+        GatherWoodTestData testData = new GatherWoodTestData();
+
+        G_Node goalNode 
+            = new G_Node(testData.npcWorldState.actionPool, testData.gatherWood.goalEffects, testData.npcWorldState);
+
+        G_Node normalNode
+            = null;
+
+        G_Node testNode = testGoalNode ? goalNode : normalNode; 
+
+        if (testGoalNode) {
+            testNode = goalNode;
+        }
+        else {
+            testNode = normalNode;
+        }
+
+        Assert.NotNull(testNode);
+        Assert.AreEqual(G_NodeState.open, testNode.NodeState);
+        Assert.AreEqual(testGoalNode, testNode.ParentNode == null);            
+        Assert.AreEqual(testGoalNode, testNode.NodeAction == null);
+        Assert.AreEqual(hCost, testNode.HCost);
+        Assert.AreEqual(unmetCount, testNode.UnmetPreconditions);
+        Assert.AreEqual(preconCount, testNode.preconditions.Count);
+
+        Assert.NotNull(testNode.preconditions);
+        Assert.AreEqual(testGoalNode, goalNode.IsGoalNode);
+    }
+    
+    [TestCase(TestName = "0 Preconditions met by worldState")]
+    [TestCase(TestName = "Some Preconditions met by worldState")]
+    public void ProcessPreconditons() {
+        
+    }
+
+    [TestCase(TestName = "Closed")]
+    [TestCase(TestName = "Success")]
+    [TestCase(TestName = "Failure")]
+    public void ProcessNode() {
+
+    }
+
+    [TestCase(TestName = "Generates several nodes")]
+    [TestCase(TestName = "Fails to generate any nodes")]
+    public void GenerateNodes() {
+
+    }
+
+    [TestCase(TestName = "Standard Plan")]
+    [TestCase(TestName = "Null action in middle")]
+    public void ReturnPlan() {
 
     }
 }
