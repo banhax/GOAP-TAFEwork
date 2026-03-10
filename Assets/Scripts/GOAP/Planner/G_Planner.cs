13c97c4d3456cdb334b4d0ff55011dcded256843
banhax <20094400@tafe.wa.edu.au>
2026-03-10 15:09:50 +0800

    worked on the G_Planner, and wrote the pseudocode

--- Assets/Scripts/GOAP/Planner/G_Planner.cs
+++ Assets/Scripts/GOAP/Planner/G_Planner.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,24 +8,48 @@ namespace GOAP {
             bool success = false;
             plan = new List<G_Action>();
 
-            // initialise the node pool
-            // create a node for the goal
-            // add node to node pool
+            List<G_Node> nodePool = new List<G_Node>();
 
-            //while plan not found
-            //  find cheapest node
-            //  process node
-            //  if node is successful
-            //      return plan
-            //      break from loop
-            //  else if plan failed
-            //      return empty plan
-            //      break from loop
-            //  else
-            //      generate child nodes
-            //      continue loop
+            G_Node rootNode = new G_Node(worldState.actionPool, goal.goalEffects, worldState);
+
+            nodePool.Add(rootNode);
+
+            G_Node currentNode = null;
+
+            while (true) {
+                currentNode = nodePool[0];
+                currentNode.ProcessNode();
+
+                if (currentNode.NodeState == G_NodeState.success) {
+                    success = true;
+
+                    plan = currentNode.ReturnPlan();
+                    if (plan == null) {
+                        success = false;
+                    }
+                    break;
+                }
+                else if (currentNode.NodeState == G_NodeState.fail) {
+                    success = false;
+                    break;
+                }
+                else if (currentNode.NodeState == G_NodeState.closed) {
+                    nodePool.AddRange(currentNode.GenerateChildNodes());
+                    nodePool = SortPool(nodePool);
+                    if (nodePool[0].NodeState == G_NodeState.open) {
+                        success = false;
+                        break;
+                    }
+                }
+            }
 
             return success;
+        }
+
+        public static List<G_Node> SortPool(List<G_Node> pool) {
+            return pool.OrderBy((node) => node.NodeState)
+                .ThenBy((node) => node.HCost)
+                .ToList();
         }
     }
 }
