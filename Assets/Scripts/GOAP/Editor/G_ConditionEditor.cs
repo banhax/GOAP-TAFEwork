e53daab3acacd859e57e09375d966b58a6bdbf63
banhax <20094400@tafe.wa.edu.au>
2026-04-01 10:29:00 +0800

    started work on customer editor stuff, finished making custom editor for conditions that now take states successfully

--- Assets/Scripts/GOAP/Editor/G_ConditionEditor.cs
+++ Assets/Scripts/GOAP/Editor/G_ConditionEditor.cs
@@ -4,17 +4,65 @@ using GOAP;
 
 [CustomPropertyDrawer(typeof(G_Condition))]
 public class G_ConditionEditor : PropertyDrawer { // only one instance of a custom property drawer running
-    bool active = false;
+
+    float height = 0f;
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-        return base.GetPropertyHeight(property, label);
+        return (base.GetPropertyHeight(property, label) + EditorGUIUtility.standardVerticalSpacing) * 2;
     }
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        height = 0f;
         SerializedProperty active = property.FindPropertyRelative("editorActive");
 
-        active.boolValue =
-            EditorGUI.Foldout(new Rect(position.x, position.y, position.width, base.GetPropertyHeight(property, label)),
-            active.boolValue,
+        active.boolValue = // being assigned based on return value of Foldout
+            EditorGUI.Foldout(GetFormattedRect(position, property, label),
+            active.boolValue, // the one that is drawn
             "Condition");
+
+        IncrementHeight(out height, property, label);
+
+        int originalIndent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel += 1;
+
+        if (active.boolValue) {
+            BuildEditor(position, property, label);
+        }
+
+        EditorGUI.indentLevel = originalIndent;
+    }
+
+    void BuildEditor(Rect position, SerializedProperty property, GUIContent label) {
+        SerializedProperty stateProperty = property.FindPropertyRelative("state");
+        //Object preStateObject = stateProperty.objectReferenceValue;
+
+        EditorGUI.ObjectField(GetFormattedRect(position, property, label), stateProperty);
+
+        IncrementHeight(out height, property, label);
+
+        if (stateProperty.objectReferenceValue != null) {
+            // build custom editor
+        }
+    }
+
+    public Rect GetFormattedRect(Rect position, SerializedProperty property, GUIContent label) {
+        return new Rect(position.x,
+            position.y + height,
+            position.width,
+            base.GetPropertyHeight(property, label));
+    }
+
+    /// <summary>
+    /// Increases local height variable by one standard property height + one standard vertical spacing gap
+    /// every time it is called so that we can increment the height variable to continue drawing elements
+    /// without them being drawn on top of each other
+    /// </summary>
+    /// <param name="progressiveHeight"></param>
+    /// <param name="property"></param>
+    /// <param name="label"></param>
+    public void IncrementHeight(out float progressiveHeight, SerializedProperty property, GUIContent label) {
+        progressiveHeight = height;
+
+        progressiveHeight += base.GetPropertyHeight(property, label);
+        progressiveHeight += EditorGUIUtility.standardVerticalSpacing;
     }
 }
