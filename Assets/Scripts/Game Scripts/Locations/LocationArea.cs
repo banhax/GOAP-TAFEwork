2e91fd1083ede0dd905b7772da756d46e7290b0b
banhax <20094400@tafe.wa.edu.au>
2026-04-02 16:31:51 +0800

    made locations instances and prefab

---
+++ Assets/Scripts/Game Scripts/Locations/LocationArea.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LocationArea : MonoBehaviour
+{
+    [SerializeField] LocationInstance parentInstance;
+
+    public LocationInstance GetParentInstance() {
+        return parentInstance;
+    }
+}
---
+++ Assets/Scripts/Game Scripts/Locations/LocationInstance.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationInstance : MonoBehaviour
+{
+    [SerializeField] LocationType locationType;
+    [SerializeField] Transform accessPoint;
+
+    MapInjector mapInjector = new MapInjector();
+
+    private void Awake() {
+        mapInjector.FindAndInjectObject(transform.position, this);
+    }
+
+    public Vector3 GetAccessPoint() {
+        if (accessPoint != null) {
+            return accessPoint.position;
+        }
+        else {
+            return transform.position;
+        }
+    }
+
+    public LocationType GetLocationType() {
+        return locationType;
+    }
+}
