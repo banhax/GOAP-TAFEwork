a5eb763f67214bdf1f1162428d73b3e433d19720
banhax <20094400@tafe.wa.edu.au>
2026-04-13 14:50:56 +0800

    wrote the get and set functions for the U_Value class, which i think is now completed

--- Assets/Scripts/Utility AI/U_Value.cs
+++ Assets/Scripts/Utility AI/U_Value.cs
@@ -110,6 +110,156 @@ namespace UtilityAI {
         }
 
         #endregion
+
+        #region Calculate Utility
+        public float GetUtility() {
+            float value = 0f;
+            float variable = ReturnSourceValue(varSource, true);
+            float maximum = ReturnSourceValue(maxSource, false);
+            float percentage = 0f;
+
+            if (variable == 0f && maximum == 0f) {
+                percentage = 0f;
+            }
+            else {
+                percentage = variable / maximum;
+            }
+
+            float result = responseCurve.Evaluate(percentage);
+
+            return result;
+        }
+
+        float ReturnSourceValue(U_ValueSource sourceType, bool isVar) {
+            float value = 0f;
+
+            switch (sourceType) {
+                case U_ValueSource.Int:
+                    value = isVar ? intVar : intMax;
+                    break;
+                case U_ValueSource.Float:
+                    value = isVar ? floatVar : floatMax;
+                    break;
+                case U_ValueSource.IntState:
+                    value = isVar ? (float)intStateVar.GetValue() : (float)intStateMax.GetValue();
+                    break;
+                case U_ValueSource.FloatState:
+                    value = isVar ? (float)floatStateVar.GetValue() : (float)floatStateMax.GetValue();
+                    break;
+                case U_ValueSource.Utility:
+                    value = isVar ? utilityVar.GetUtility() : utilityMax.GetUtility();
+                    break;
+            }
+
+            return value;
+        }
+
+        #endregion
+
+        #region Set Functions
+
+        public void SetValue(bool isVar, float value) {
+            if (isVar) {
+                floatVar = value;
+            }
+            else {
+                floatMax = value;
+            }
+        }
+
+        public void SetValue(bool isVar, int value) {
+            if (isVar) {
+                intVar = value;
+            }
+            else {
+                intMax = value;
+            }
+        }
+
+        public void AssignReference(bool isVar, G_IntState value) {
+            if (isVar) {
+                intStateVar = value;
+            }
+            else {
+                intStateMax = value;
+            }
+        }
+
+        public void AssignReference(bool isVar, G_FloatState value) {
+            if (isVar) {
+                floatStateVar = value;
+            }
+            else {
+                floatStateMax = value;
+            }
+        }
+
+        public void AssignReference(bool isVar, U_Value value) {
+            if (isVar) {
+                utilityVar = value;
+            }
+            else {
+                utilityMax = value;
+            }
+        }
+
+        #endregion
+
+        public float Get(bool isVar) {
+            U_ValueSource source = isVar ? varSource : maxSource;
+            return ReturnSourceValue(source, isVar);
+        }
+
+        #region Local State Assignment
+
+        public void AssignLocalValues(List<U_Value> localU_Values, List<G_State> localStates) {
+            switch (varSource) {
+                case U_ValueSource.IntState:
+                    FindAndSetLocalValue(localStates, intStateVar, true);
+                    break;
+                case U_ValueSource.FloatState:
+                    FindAndSetLocalValue(localStates, floatStateVar, true);
+                    break;
+                case U_ValueSource.Utility:
+                    FindAndSetLocalValue(localU_Values, utilityVar, true);
+                    break;
+            }
+
+            switch (maxSource) {
+                case U_ValueSource.IntState:
+                    FindAndSetLocalValue(localStates, intStateMax, false);
+                    break;
+                case U_ValueSource.FloatState:
+                    FindAndSetLocalValue(localStates, floatStateMax, false);
+                    break;
+                case U_ValueSource.Utility:
+                    FindAndSetLocalValue(localU_Values, utilityMax, false);
+                    break;
+            }
+        }
+
+        void FindAndSetLocalValue(List<G_State> localStates, G_State refState, bool isVar) {
+            G_State foundState = localStates.Find((state) => state != null && state.name == refState.name);
+
+            if (foundState != null) {
+                if (refState is G_IntState) {
+                    AssignReference(isVar, foundState as G_IntState);
+                }
+                else if (refState is G_FloatState) {
+                    AssignReference(isVar, foundState as G_FloatState);
+                }
+            }
+        }
+
+        void FindAndSetLocalValue(List<U_Value> localU_Values, U_Value refValue, bool isVar) {
+            U_Value foundValue = localU_Values.Find((value) => value != null && value.name == refValue.name);
+
+            if (foundValue != null) {
+                AssignReference(isVar, foundValue);
+            }
+        }
+
+        #endregion
     }
 
     public enum U_ValueSource {
