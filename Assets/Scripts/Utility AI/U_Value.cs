89de687b55117dbff2af8ca5401e12dce6484ae4
banhax <20094400@tafe.wa.edu.au>
2026-04-13 14:23:49 +0800

    still working on the U_Value file, made the construction functions

--- Assets/Scripts/Utility AI/U_Value.cs
+++ Assets/Scripts/Utility AI/U_Value.cs
@@ -37,13 +37,87 @@ namespace UtilityAI {
         public U_Value UtilityMax { get { return utilityMax; } }
 
         #endregion
+
+        #region Construction Functions
+
+        public void Construct(
+            string name,
+            U_ValueSource varSource,
+            U_ValueSource maxSource,
+            AnimationCurve responseCurve,
+            int intVar = 0,
+            float floatVar = 0f,
+            G_IntState intStateVar = null,
+            G_FloatState floatStateVar = null,
+            U_Value utilityVar = null,
+            int intMax = 0,
+            float floatMax = 0f,
+            G_IntState intStateMax = null,
+            G_FloatState floatStateMax = null,
+            U_Value utilityMax = null
+            ) {
+
+            this.name = name;
+            this.responseCurve = responseCurve;
+            this.varSource = varSource;
+
+            this.intVar = intVar;
+            this.floatVar = floatVar;
+            this.intStateVar = intStateVar;
+            this.floatStateVar = floatStateVar;
+            this.utilityVar = utilityVar;
+
+            this.maxSource = maxSource;
+            this.intMax = intMax;
+            this.floatMax = floatMax;
+            this.intStateMax = intStateMax;
+            this.floatStateMax = floatStateMax;
+            this.utilityMax = utilityMax;
+        }
+
+        public U_Value Clone() {
+            U_Value clone = CreateInstance<U_Value>();
+            
+            // can use semi colons when using big constructors to pick specific arguments
+            // (as long as the others are set to some value in the constructor originally!)
+
+            //clone.Construct(
+            //    name,
+            //    varSource,
+            //    maxSource,
+            //    responseCurve,
+            //    floatVar: this.floatVar,
+            //    floatMax: this.floatMax,
+            //    utilityVar: this.utilityVar);
+
+            clone.Construct(
+                name,
+                varSource,
+                maxSource,
+                responseCurve,
+                intVar,
+                floatVar,
+                intStateVar,
+                floatStateVar,
+                utilityVar,
+                intMax,
+                floatMax,
+                intStateMax,
+                floatStateMax,
+                utilityMax);
+
+            return clone;
+        }
+
+        #endregion
+    }
+
+    public enum U_ValueSource {
+        Int,
+        Float,
+        IntState,
+        FloatState,
+        Utility
     }
 }
 
-public enum U_ValueSource {
-    Int,
-    Float,
-    IntState,
-    FloatState,
-    Utility
-}
